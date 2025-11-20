c1a575b2d56528ca230761dad57238ac3ce5800f
AndersonSantos111 <anderson9940381@gmail.com>
2025-11-20 12:24:10 -0300

    Adicionado sistema de pontuação
    Código modificado usando dicionário criando  tabela de forma mais dinâmica e adicionando sistema com pontuação.

--- jogo.py
+++ jogo.py
@@ -1,69 +1,100 @@
 import random
+import os
 
+# VARIAVEIS:
+limite_chances = 3      ## chances por rodada
+limite_rodadas = 5      ## número maximo de rodadas
+# rodada_atual = 1        ## começa na rodada 1
 
+tabela_pontos = {'chaces_restantes': limite_chances, 'rodada_atual': 0, 'pontuação': 0}
 
-def gerarNumero():
-    return random.randint(0, 20)
+# FUNCOES:
+def gerarNumero(): # Solteia um numero
+    return random.randint(0, 10)
 
-def pedirEntrada():
+numero = gerarNumero() # Recolhe o numero sorteado 
+
+def pedirEntrada(): # Recolhendo entrada do player
     entrada = input('Chute um número (ou digite "pare" para encerrar): ').strip()
     return entrada
 
-
-
-limite_chances = 3      ## chances por rodada
-limite_rodadas = 5      ## número maximo de rodadas
-rodada_atual = 1        ## começa na rodada 1
-
-numero = gerarNumero()
-chances_restantes = limite_chances
-
-while rodada_atual <= limite_rodadas:
-
-    print(f"Rodada {rodada_atual} de {limite_rodadas}")
-    print(f"Chances restantes: {chances_restantes}")
-
-    entrada = pedirEntrada()
-
- 
-    if entrada.lower() == "pare":
-        print("Jogo encerrado pelo usuário.")
-        break
-
-   
-    if entrada == "":
-        print("Você não digitou nada.")
-        continue
-
-    if not entrada.isdigit():
-        print("Digite apenas números!")
-        continue
-
-    NumeroUsuario = int(entrada)
-
-    if NumeroUsuario > numero:
+def atualizar_tabela(NumeroUsuario, N): # Atualiza dados da tabela
+    os.system('cls')
+    if NumeroUsuario > N: # Checagem de acertos
         print(f"É menor que {NumeroUsuario}")
-        chances_restantes -= 1
+        #chances_restantes -= 1
+        tabela_pontos["chaces_restantes"] -= 1
 
-    elif NumeroUsuario < numero:
-        print(f"É maior que {NumeroUsuario}")
-        chances_restantes -= 1
+    elif NumeroUsuario < N:
+        # chances_restantes -= 1
+        tabela_pontos["chaces_restantes"] -= 1
+        
 
     else:
         print("Acertou seu cabeçudo")
        
-        numero = gerarNumero()
-        chances_restantes = limite_chances
+        # numero = gerarNumero()
+        # print(numero)
+        if tabela_pontos["chaces_restantes"] == 3:
+            tabela_pontos["pontuação"] += 3
+
+        if tabela_pontos["chaces_restantes"] == 2:
+            tabela_pontos["pontuação"] += 2
+
+        if tabela_pontos["chaces_restantes"] == 1:
+            tabela_pontos["pontuação"] += 1
+
+        tabela_pontos["chaces_restantes"] = limite_chances
        
-        rodada_atual += 1
+        tabela_pontos["rodada_atual"] += 1
+        return True
+
+
+
+
+
+
+
+
+
+
+# GAME:
+
+while tabela_pontos["rodada_atual"] <= limite_rodadas:
+
+    # print(f"|Rodada {rodada_atual} de {limite_rodadas}|")
+    # print(f"|Chances restantes: {chances_restantes}|")
+    print(tabela_pontos)
+
+    entrada = pedirEntrada()
+
+ 
+    if entrada.lower() == "pare": # Encerra o jogo
+        os.system('cls')
+        print("Jogo encerrado pelo usuário.")
+        break
+
+   
+    if entrada == "": # Checa se digiotou algo
+        os.system('cls')
+        print("Você não digitou nada.")
+        continue
+
+    if not entrada.isdigit(): # Checa se digitou um número
+        os.system('cls')
+        print("Digite apenas números!")
+        continue
+
+    if(atualizar_tabela(int(entrada), numero)): # Atualizar tabela
         continue
 
     
-    if chances_restantes == 0:
+    if tabela_pontos["chaces_restantes"] == 0:
+        os.system('cls')
         print(f"\n cabosse! O número era {numero}.")
     
-        chances_restantes = limite_chances
+        tabela_pontos["chaces_restantes"] = limite_chances
         numero = gerarNumero()
-        rodada_atual += 1
+        tabela_pontos["rodada_atual"] += 1
 
 print("\nGAME OVER!")
